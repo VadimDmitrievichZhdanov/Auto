8ffd2860001bbdb7fd2d39b973b732bc0cb5d21f
Vadim <Vadimzhdanov@mail.ru>
2022-10-31 17:22:19 +0300

    not finished

---
+++ Auto/src/OOPpart2/s45.java
@@ -0,0 +1,33 @@
+package OOPpart2;
+
+import java.util.Scanner;
+
+//● Доработайте пример с оператором if… else (слайд 45) - если пользователь
+//вводит значение вне диапазона 0 - 100, вывести сообщение “Введено
+//некорректное значение” и попросить повторить ввод (использовать цикл)
+//if … else
+//слайд 45:
+//System.out.print(“Please enter your score: “;
+//int score = new Scanner(System.in).nextInt();
+//if (score >= 70) {
+//System.out.println(“Congrats! You’ve passed the test!”);
+//} else {
+//System.out.println(“Sorry, you’ve failed the test.”);
+//}
+public class s45 {
+    public static void main(String[] args) {
+
+        System.out.print("Please enter your score: ");
+        int score = new Scanner(System.in).nextInt();
+      //  for  ((score < 0) | (score > 100)) {
+            System.out.println("incorrect value");
+            System.out.print("Please enter your score: (0 - 100) ");
+
+            if (score >= 70) {
+                System.out.println("Congrats! You’ve passed the test!");
+            } else {
+                System.out.println("Sorry, you’ve failed the test.");
+            }
+        }
+    }
+

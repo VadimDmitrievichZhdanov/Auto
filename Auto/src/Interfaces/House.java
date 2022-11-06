07f021fca7eaa37f1afbc8102103f4b0c644be02
Vadim <Vadimzhdanov@mail.ru>
2022-11-06 09:25:14 +0300

    Homework

---
+++ Auto/src/Interfaces/House.java
@@ -0,0 +1,9 @@
+package Interfaces;
+
+public class House extends Rectangle{
+    public static void main(String[] args) {
+        Rectangle Marksa58 = new Rectangle(50,30);
+        double Marksa58area = Marksa58.getArea();
+        System.out.println((int)Marksa58area + "m");
+    }
+}

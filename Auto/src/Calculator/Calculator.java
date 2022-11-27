7a95fdbf44fef96b171451402e231edcf0c013d8
Вадим <Vadimzhdanov@mail.ru>
2022-11-27 11:01:16 +0300

    undone

--- Auto/src/Calculator/Calculator.java
+++ Auto/src/Calculator/Calculator.java
@@ -1,10 +1,47 @@
 package Calculator;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.math.BigInteger;
 import java.security.InvalidParameterException;
+import java.util.Properties;
+import java.util.Scanner;
 
 public class Calculator {
-    private double result; // TODO: Impl me!
+    public static void main(String[] args) {
+
+        double result; // TODO: Impl me!
+        FileWriter fWriter = null;
+        try {
+            File outputFile = new File("CalculatorOutput.txt");
+            fWriter = new FileWriter(outputFile, true);
+            Scanner scanner = new Scanner(outputFile);
+
+            while (scanner.hasNextLine()) {
+                System.out.println(scanner.nextLine());
+            }
+
+            Properties props = new Properties();
+            FileInputStream inputStream = new FileInputStream("config.properties");
+            props.load(inputStream);
+
+
+        } catch (IOException ex) {
+            System.out.println(ex.toString());
+        } catch (Exception ex) {
+            System.out.println(ex.toString());
+        } finally {
+            try {
+                if (fWriter != null) {
+                    fWriter.close();
+                }
+            } catch (IOException exception) {
+                System.out.println(exception.toString());
+            }
+        }
+    }
 
     public double add(double a, double b) {
         return a + b;
@@ -29,5 +66,3 @@ public class Calculator {
         return a.pow(b);
     }
 }
-
-

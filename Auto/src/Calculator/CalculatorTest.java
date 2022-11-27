c0cf4893e32a6d74fac9fbfac30c8da8fc3ec38b
Вадим <Vadimzhdanov@mail.ru>
2022-11-27 09:33:39 +0300

    UnitTest

---
+++ Auto/src/Calculator/CalculatorTest.java
@@ -0,0 +1,31 @@
+package Calculator;
+
+import java.math.BigInteger;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+    private final Calculator calculator = new Calculator();
+
+    @org.junit.jupiter.api.Test
+    void addTest() {assertEquals(2, calculator.add(1, 1));
+    }
+
+    @org.junit.jupiter.api.Test
+    void sub() {assertEquals(0, calculator.sub(1, 1));
+    }
+
+    @org.junit.jupiter.api.Test
+    void mul() {assertEquals(20, calculator.mul(4, 5));
+    }
+
+    @org.junit.jupiter.api.Test
+    void div() { assertEquals(2, calculator.div(10, 5));
+    }
+
+    @org.junit.jupiter.api.Test
+    void pow() { assertEquals(BigInteger.valueOf(3125), calculator.pow(BigInteger.valueOf(5), 5));
+    }
+
+
+}
